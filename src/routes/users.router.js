3bc4131266007ee419769aa44a5d8b734a4a5270
unknown <luis.daniel.tang@gmail.com>
2023-02-07 23:24:11 -0300

    addAuth

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import * as UserController from "../controllers/users.controller.js";
+import { auth } from "../middleware/auth.middleware.js";
 const router=Router();
 
-
+router.post("/", UserController.createUser);
+router.get("/:email", auth, UserController.getUser);
 
 
 export default router;
